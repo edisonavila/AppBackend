b628c3418ef78a403bf0c027cb8492a112b25b33
cajas <cajas@DESKTOP-SUNK2VO>
2021-10-11 12:06:11 -0500

    Update 2.5

--- ciclo3back/src/main/java/co/edu/unbosque/ciclo3back/api/VentasAPI.java
+++ ciclo3back/src/main/java/co/edu/unbosque/ciclo3back/api/VentasAPI.java
@@ -1,12 +1,16 @@
 package co.edu.unbosque.ciclo3back.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.unbosque.ciclo3back.dao.VentasDAO;
+import co.edu.unbosque.ciclo3back.model.Usuarios;
 import co.edu.unbosque.ciclo3back.model.Ventas;
 
 @RestController
@@ -19,6 +23,11 @@ public class VentasAPI {
 	@PostMapping("/guardar")
 	public void guardar(@RequestBody Ventas ventas) {
 		ventasdao.save(ventas);
+	}
+	
+	@GetMapping("/listar")
+	public List<Ventas>listar(){
+		return ventasdao.findAll();
 	}
 	
 }
